788def803762f0a193315fd7ae7c9366e18fd46c
Albert <albert@usv.com>
2020-12-05 15:17:21 -0500

    Fix gmail bookmarklet

--- pages/gmailbookmark.js
+++ pages/gmailbookmark.js
@@ -2,40 +2,41 @@ class GmailBookmark extends React.Component {
 
 	static async getInitialProps({ query, res }) {
 	    
-	    // http://localhost:3000/gmailbookmark?partner=rebecca
-	    const partner = query.partner ? query.partner : ""
+	    // http://localhost:3000/gmailbookmark?partner=Rebecca
+	    const partner = query.partner ? query.partner : null
+	    console.log(partner)
 
-	    const url = `https://api.airtable.com/v0/${process.env.AIRTABLE_COMPANIES_BASE_ID}/Companies?filterByFormula=Find(%22${partner}%22%2C+partner)`
+	    const urlBase = `https://api.airtable.com/v0/${process.env.AIRTABLE_COMPANIES_BASE_ID}/Companies`
+	    const url = partner ? urlBase + "?filterByFormula=" + encodeURIComponent('partner="' + partner + '"') : urlBase
+	    console.log(url)
 
 	    const resCompanies = await fetch(url, { headers: { "Authorization": `Bearer ${process.env.AIRTABLE_KEY}` } })
 	    const jsonCompanies = await resCompanies.json()
 
 	    // return as props
-	    let companies = null
-	  	companies = jsonCompanies.records ? jsonCompanies.records : null   
+	    let companies = jsonCompanies.records ? jsonCompanies.records : null   
 
-	  	let redirectString = "https://mail.google.com/mail/u/0/#search/is%3Aimportant+from%3A("
+	  	let params = "is:important ("
+		let join = ""
 
-  		for (const i in companies) {
-  			if (companies[i].fields.domain) {
-	  			redirectString += companies[i].fields.domain + " OR "	
-  			}
-  			if (companies[i].fields.alternate_domains) {
-  				let ads = companies[i].fields.alternate_domains.split(",")
-  				for (const j in ads) {
-  					redirectString += ads[j] + " OR "	
-  				}
+  		for (var i in companies) {
+  			if (companies[i].fields.company_name) {
+	  			params += join + '"' + companies[i].fields.company_name + '"'
+                                join = " OR "	
   			}
   		}
 
-  		redirectString += ")"
+  		let redirectString = "https://mail.google.com/mail/u/0/#search/" + encodeURIComponent(params)  + ")"
+		// console.log(redirectString)
 
+		/*
 	  	if (res) {
 		    res.writeHead(301, {
 		      Location: redirectString
 		    });
 		    res.end();
-		  } 
+		  }
+		*/ 
 
 	    return {
 	    	companies: companies,
